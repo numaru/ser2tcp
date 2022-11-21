1d5c5df6fe62d7c9615f8eef7ab3a90ca266ddb3
numaru <kin.numaru@protonmail.com>
2022-11-21 07:37:10 +0100

    :bug: Fix the deadlock in serial message reception
    Fix by using the tokio_serial instead of the serialport-rs crate.
    Also mark some arguments required and use tcp-port instead of
    tcp-address since the first part of the address might always be 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use clap::{arg, command, Command};
 use std::sync::atomic::{AtomicUsize, Ordering};
 use tokio::{
-    io::{self, AsyncReadExt, AsyncWriteExt},
+    io::{
+        self, AsyncReadExt, AsyncWriteExt,
+        ErrorKind::{BrokenPipe, TimedOut},
+    },
     net::TcpListener,
     sync::broadcast,
 };
+use tokio_serial::SerialPortBuilderExt;
 
 fn cli() -> Command {
     command!().args(&[
-        arg!(--"serial-port" <SERIAL_PORT> "The device path to a serial port"),
+        arg!(--"serial-port" <SERIAL_PORT> "The device path to a serial port").required(true),
         arg!(--baudrate <BAUDRATE> "The baudrate to connect at")
-            .value_parser(clap::value_parser!(u32).range(9600..)),
-        arg!(--"tcp-address" <TCP_ADDRESS> "The address to bind the tcp server on"),
+            .value_parser(clap::value_parser!(u32).range(9600..))
+            .required(true),
+        arg!(--"tcp-port" <TCP_PORT> "The port to bind the tcp server on")
+            .value_parser(clap::value_parser!(u32))
+            .required(true),
     ])
 }
 
@@ -26,30 +33,44 @@ async fn main() -> io::Result<()> {
 
     let serial_port = matches.get_one::<String>("serial-port").unwrap().clone();
     let baudrate = *matches.get_one::<u32>("baudrate").unwrap();
-    let tcp_address = matches.get_one::<String>("tcp-address").unwrap().clone();
+    let tcp_port = matches.get_one::<u32>("tcp-port").unwrap();
 
-    let listener = TcpListener::bind(tcp_address).await?;
-    let (tx_base, _) = broadcast::channel::<(Vec<u8>, usize)>(16);
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", tcp_port)).await?;
+    let (base_tx, _) = broadcast::channel::<(Vec<u8>, usize)>(256);
 
-    let tx = tx_base.clone();
+    let tx = base_tx.clone();
     let mut rx = tx.subscribe();
 
     tokio::spawn(async move {
         let self_uid = generate_uid();
-        let mut serial_port = serialport::new(serial_port, baudrate).open().unwrap();
-        tokio::select! {
-            result = async {
-                let mut buffer: Vec<u8> = vec![0; 1024];
-                serial_port.read(&mut buffer)?;
-                io::Result::<Vec<u8>>::Ok(buffer)
-             } => {
-                let buffer = result.unwrap();
-                tx.send((buffer, self_uid)).unwrap();
-            }
-            result = rx.recv() => {
-                let (msg, rx_uid) = result.unwrap();
-                if rx_uid != self_uid {
-                    serial_port.write(&msg).unwrap();
+        let mut serial_port = tokio_serial::new(serial_port, baudrate)
+            .open_native_async()
+            .unwrap();
+        let mut buffer = [0; 1024];
+        loop {
+            tokio::select! {
+                result = serial_port.read(&mut buffer) => {
+                    match result {
+                        Ok(n) if n == 0 => {
+                            // TODO: close the serial_port
+                            println!("close the serial_port");
+                        },
+                        Ok(n) => {
+                            tx.send((buffer[0..n].to_vec(), self_uid)).unwrap();
+                        },
+                        Err(ref e) if e.kind() == TimedOut => (),
+                        Err(ref e) if e.kind() == BrokenPipe => (),
+                        Err(e) => {
+                            println!("{:?}", e);
+                        },
+                    };
+                }
+                result = rx.recv() => {
+                    let (msg, rx_uid) = result.unwrap();
+                    if rx_uid != self_uid {
+                        serial_port.write(&msg).await.unwrap();
+                        serial_port.flush().await.unwrap();
+                    }
                 }
             }
         }
@@ -58,26 +79,36 @@ async fn main() -> io::Result<()> {
     loop {
         let (mut socket, _) = listener.accept().await?;
 
-        let tx = tx_base.clone();
+        let tx = base_tx.clone();
         let mut rx = tx.subscribe();
 
+        let self_uid = generate_uid();
+
         tokio::spawn(async move {
-            let self_uid = generate_uid();
+            println!("uid: {}", self_uid);
             let (mut reader, mut writer) = socket.split();
+            let mut buffer = [0; 1024];
             loop {
                 tokio::select! {
-                    result = async {
-                        let mut buffer: Vec<u8> = vec![0; 1024];
-                        reader.read(&mut buffer).await?;
-                        io::Result::<Vec<u8>>::Ok(buffer)
-                     } => {
-                        let buffer = result.unwrap();
-                        tx.send((buffer, self_uid)).unwrap();
+                    result = reader.read(&mut buffer) => {
+                        match result {
+                            Ok(n) if n == 0 => {
+                                // TODO: close the socket
+                                println!("close the socket");
+                            },
+                            Ok(n) => {
+                                tx.send((buffer[0..n].to_vec(), self_uid)).unwrap();
+                            },
+                            Err(e) => {
+                                println!("{:?}", e);
+                            },
+                        };
                     }
                     result = rx.recv() => {
                         let (msg, rx_uid) = result.unwrap();
                         if rx_uid != self_uid {
                             writer.write(&msg).await.unwrap();
+                            writer.flush().await.unwrap();
                         }
                     }
                 }
